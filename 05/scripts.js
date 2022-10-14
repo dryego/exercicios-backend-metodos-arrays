bcad3a9db6bec1a8fa32ca8ebede94684b504713
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-13 21:51:23 -0300

    Resolução, Fila 2.0.

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,36 @@
-const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+const pacientes = ['José', 'Pedro', 'Maria', 'Dryego', 'João', 'Ana', 'Bárbara', 'Joana'];
+const atendimento = "atender"; //"atender" "agendar";
+const nome = "Dryego";
+
+function fila(pacientes) {
+
+    const formatarLista = pacientes.join(", ");
+
+    return formatarLista;
+}
+function agendarPaciente(pacientes, nome) {
+    pacientes.push(nome);
+    const formatarLista = pacientes.join(", ");
+
+    return formatarLista;
+
+}
+function atenderPaciente(pacientes) {
+    pacientes.shift();
+    const formatarLista = pacientes.join(", ");
+
+    return formatarLista;
+}
+function cancelarAtendimento(pacientes, nome) {
+    //const posiçãoNaFila = pacientes.indexOf("nome");
+    //if (posiçãoNaFila > -1) {
+    pacientes.splice(pacientes.indexOf(nome), 1);
+    //}
+    const formatarLista = pacientes.join(", ");
+
+    return formatarLista;
+}
+
+console.log(agendarPaciente(pacientes, nome));
+console.log(atenderPaciente(pacientes));
+console.log(cancelarAtendimento(pacientes, nome));
