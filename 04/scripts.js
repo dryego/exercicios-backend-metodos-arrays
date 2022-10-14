15d127127a94ff21e9991f482b930513cf336c2b
dryego barbosa <dryego.lisboa@gmail.com>
2022-10-13 21:05:45 -0300

    Resolução, Fila de pacientes.

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,16 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+const atendimento = "atender"; //"atender" "agendar";
+const nome = "Dryego";
+
+function fila(pacientes, atendimento, nome) {
+    if (atendimento == "atender") {
+        pacientes.shift();
+    } else if (atendimento == "agendar") {
+        pacientes.push(nome);
+    }
+    const formatarLista = pacientes.join(", ");
+
+    return formatarLista;
+}
+
+console.log(fila(pacientes, atendimento, nome));
